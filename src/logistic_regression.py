22d9ccd60e7057af2e6cc52fe3311967bc2bcaf4
hoppm <hoppm@uw.edu>
2018-02-19 22:22:28 -0800

    Adding logistic regression notes

---
+++ src/logistic_regression.py
@@ -0,0 +1,17 @@
+# Logistic Regression Classifier
+import numpy as np
+
+class LogisticRegressionClassifier:
+	
+	def __init__(self):
+		pass
+
+	def sigmoid(x):
+		return 1. / (1. + np.exp(-x))
+
+	def train(data, targets):
+		pass
+
+	def score(test, targets):
+		pass
+
